20b4e4ac600e27c3b7a4baf7bfffbd565fb01991
Ajsa <besirovicajsa@gmail.com>
2024-10-28 20:20:49 +0100

    Creating Music album page

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import Navbar from "./Navbar";
+import AlbumItem from "./AlbumItem";
+import { albumsData, songsData } from "../assets/assets";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
+      <div className="mb-4 ">
+        <h1>Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4 ">
+        <h1>Today's Biggest Hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
