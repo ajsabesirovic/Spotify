3a82dc4969106f7fbf7094ba8752a8629d9d6fb6
Ajsa <besirovicajsa@gmail.com>
2024-10-30 19:53:40 +0100

    Last touches

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,15 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
 import { albumsData, assets, songsData } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayAlbum = () => {
-  useEffect(() => {
-    console.log(id, "use");
-  }, []);
   const { id } = useParams();
   const albumData = albumsData[id];
-  console.log(albumData);
+  const { playWithId } = useContext(PlayerContext);
+
   return (
     <>
       <Navbar />
@@ -42,13 +41,18 @@ const DisplayAlbum = () => {
       <hr />
       {songsData.map((item, index) => (
         <div
+          onClick={() => playWithId(item.id)}
           key={index}
           className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
         >
           <p className="text-white">
             <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
-            <img src={item.image} alt="" />
+            <img className="inline w-10 mr-5" src={item.image} alt="" />
+            {item.name}
           </p>
+          <p className="text-[15px]">{albumData.name}</p>
+          <p className="text-[15px] hidden sm:block ">5 days ago</p>
+          <p className="text-[15px] text-center">{item.duration}</p>
         </div>
       ))}
     </>
